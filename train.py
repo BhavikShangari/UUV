a6e9fc407a9a5801b3e292300b6784b04cb5fd28
bhavikshangari <bhavikshangari@gmail.com>
2024-03-08 00:36:52 +0530

    wavenet BETTER(Precision, Recall) POOR(Box, Class loss)

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 model = YOLO("yolov8n.pt")  # build a new model from scratch
 # Use the model
-model.train(data="data.yaml", epochs=100, batch = 8, device=1)
+model.train(data="data.yaml", epochs=100, batch = 8, device=0)
